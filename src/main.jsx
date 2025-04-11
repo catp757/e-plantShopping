f91bfa6377945a1ff71463aabebee9b932727f0b
catp757 <158765774+catp757@users.noreply.github.com>
2025-04-11 00:25:34 -0400

    Added HashRouter to main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+import { HashRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <HashRouter> 
+        <App />
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
